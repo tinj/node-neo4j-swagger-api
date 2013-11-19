5d32ac04b6d1f3437e747022ac7b3500e2e09d07
Mat Tyndall <mat.tyndall@gmail.com>
2013-11-18 17:19:09 -0800

    removed unnecessary encapsulation, moved Cypher helpers to a separate module

--- models/users.js
+++ models/users.js
@@ -6,212 +6,181 @@
 
 var _ = require('underscore');
 var uuid = require('uuid');
-var cypher = require('../neo4j');
+var Cypher = require('../neo4j');
 var User = require('../models/neo4j/user');
 
-module.exports = (function () {
 
-  /**
-   *  Util Functions
-   */
+/**
+ *  Result Functions
+ *  to be combined with queries using _.partial()
+ */
 
-  // for creating cypher queries and processing the results
-  function Cypher (queryFn, resultsFn) {
-    return function (params, options, callback) {
-      queryFn(params, options, function (err, query, cypher_params) {
-        if (err) return callback(err);
-        cypher(query, cypher_params, function (err, results) {
-          if (err || !results.length || !_.isFunction(resultsFn)) return callback(err);
-          resultsFn(results, callback);
-        });
-      });
-    };
-  }
+// return a single user
+var _singleUser = function (results, callback) {
+  callback(null, new User(results[0].user));
+};
 
-  var whereTemplate = _.template('user.<%= key %>={<%= key %>}');
+// return multiple users
+var _multipleUsers = function (results, callback) {
+  var users = _.map(results, function (result) {
+    return new User(result.user);
+  });
 
-  var _where = function (keys) {
-    if (keys && keys.length) {
-      return 'WHERE '+_.map(keys, function (key) {
-        return whereTemplate({key: key});
-      }).join(' AND ');
-    }
+  callback(null, users);
+};
+
+// returns a user and a friend
+var _singleUserWithFriend = function (results, callback) {
+  callback(null, new User(results[0].user), new User(results[0].friend));
+};
+
+// returns a user and their friends
+var _singleUserWithFriends = function (results, callback) {
+  var user = new User(results[0].user)
+  var friends = _.map(results[0].friends, function (friend) {
+    return new User(friend);
+  });
+  user.friends(friends);
+  callback(null, user);
+};
+
+/**
+ *  Query Functions
+ *  to be combined with result functions using _.partial()
+ */
+
+
+var _matchBy = function (keys, params, options, callback) {
+  var cypher_params = _.pick(params, keys);
+
+  var query = [
+    'MATCH (user:User)',
+    Cypher.where('user', keys),
+    'RETURN user'
+  ].join('\n');
+
+  callback(null, query, cypher_params);
+};
+
+var _matchByUUID = _.partial(_matchBy, ['uuid']);
+var _matchByName = _.partial(_matchBy, ['name']);
+var _matchAll = _.partial(_matchBy, []);
+
+
+var _updateName = function (params, options, callback) {
+  var cypher_params = {
+    uuid : params.uuid,
+    name : params.name
   };
 
+  var query = [
+    'MATCH (user:User)',
+    'WHERE user.uuid = {uuid}',
+    'SET user.name = {name}',
+    'RETURN user'
+  ].join('\n');
 
-  /**
-   *  Result Functions
-   *  to be combined with queries using _.partial()
-   */
+  callback(null, query, cypher_params);
+};
 
-  // return a single user
-  var _singleUser = function (results, callback) {
-    callback(null, new User(results[0].user));
+// creates the user with cypher
+var _create = function (params, options, callback) {
+  var cypher_params = {
+    uuid: params.uuid || uuid(),
+    name: params.name
   };
 
-  // return multiple users
-  var _multipleUsers = function (results, callback) {
-    var users = _.map(results, function (result) {
-      return new User(result.user);
-    });
+  var query = [
+    'MERGE (user:User {name: {name}, uuid: {uuid}})',
+    'ON CREATE user',
+    'SET user.created = timestamp()',
+    'ON MATCH user',
+    'SET user.lastLogin = timestamp()',
+    'RETURN user'
+  ].join('\n');
 
-    callback(null, users);
+  callback(null, query, cypher_params);
+};
+
+// delete the user and any relationships with cypher
+var _delete = function (params, options, callback) {
+  var cypher_params = {
+    uuid: params.uuid
   };
 
-  // returns a user and a friend
-  var _singleUserWithFriend = function (results, callback) {
-    callback(null, new User(results[0].user), new User(results[0].friend));
+  var query = [
+    'MATCH (user:User)',
+    'WHERE user.uuid={uuid}',
+    'WITH user',
+    'MATCH (user)-[r?]-()',
+    'DELETE user, r',
+  ].join('\n');
+  callback(null, query, cypher_params);
+};
+
+
+// friend the user
+var _friend = function (params, options, callback) {
+  var cypher_params = {
+    uuid: params.uuid,
+    friend: params.friend
   };
 
-  // returns a user and their friends
-  var _singleUserWithFriends = function (results, callback) {
-    var user = new User(results[0].user)
-    var friends = _.map(results[0].friends, function (friend) {
-      return new User(friend);
-    });
-    user.friends(friends);
-    callback(null, user);
+  var query = [
+    'MATCH (user:User), (friend:User)',
+    'WHERE user.uuid={uuid} AND friend.uuid={friend} AND NOT((user)-[:friend]-(friend))',
+    'CREATE (user)-[:friend {created: timestamp()}]->(friend)',
+    'RETURN user, friend'
+  ].join('\n');
+  callback(null, query, cypher_params);
+};
+
+// unfriend the user
+var _unfriend = function (params, options, callback) {
+  var cypher_params = {
+    uuid: params.uuid,
+    friend: params.friend
   };
 
-  /**
-   *  Query Functions
-   *  to be combined with result functions using _.partial()
-   */
+  var query = [
+    'MATCH (user:User)-[r:friend]-(friend:User)',
+    'WHERE user.uuid={uuid} AND friend.uuid={friend}',
+    'DELETE r',
+    'RETURN user, friend'
+  ].join('\n');
+  callback(null, query, cypher_params);
+};
 
-
-  var _matchBy = function (keys, params, options, callback) {
-    var cypher_params = _.pick(params, keys);
-
-    var query = [
-      'MATCH (user:User)',
-      _where(keys),
-      'RETURN user'
-    ].join('\n');
-
-    callback(null, query, cypher_params);
+// match with friends
+var _matchWithFriends = function (params, options, callback) {
+  var cypher_params = {
+    uuid: params.uuid
   };
 
-  var _matchByUUID = _.partial(_matchBy, ['uuid']);
-  var _matchByName = _.partial(_matchBy, ['name']);
-  var _matchAll = _.partial(_matchBy, []);
-
-
-  var _updateName = function (params, options, callback) {
-    var cypher_params = {
-      uuid : params.uuid,
-      name : params.name
-    };
-
-    var query = [
-      'MATCH (user:User)',
-      'WHERE user.uuid = {uuid}',
-      'SET user.name = {name}',
-      'RETURN user'
-    ].join('\n');
-
-    callback(null, query, cypher_params);
-  };
-
-  // creates the user with cypher
-  var _create = function (params, options, callback) {
-    var cypher_params = {
-      uuid: params.uuid || uuid(),
-      name: params.name
-    };
-
-    var query = [
-      'MERGE (user:User {name: {name}, uuid: {uuid}})',
-      'ON CREATE user',
-      'SET user.created = timestamp()',
-      'ON MATCH user',
-      'SET user.lastLogin = timestamp()',
-      'RETURN user'
-    ].join('\n');
-
-    callback(null, query, cypher_params);
-  };
-
-  // delete the user and any relationships with cypher
-  var _delete = function (params, options, callback) {
-    var cypher_params = {
-      uuid: params.uuid
-    };
-
-    var query = [
-      'MATCH (user:User)',
-      'WHERE user.uuid={uuid}',
-      'WITH user',
-      'MATCH (user)-[r?]-()',
-      'DELETE user, r',
-    ].join('\n');
-    callback(null, query, cypher_params);
-  };
-
-
-  // friend the user
-  var _friend = function (params, options, callback) {
-    var cypher_params = {
-      uuid: params.uuid,
-      friend: params.friend
-    };
-
-    var query = [
-      'MATCH (user:User), (friend:User)',
-      'WHERE user.uuid={uuid} AND friend.uuid={friend} AND NOT((user)-[:friend]-(friend))',
-      'CREATE (user)-[:friend {created: timestamp()}]->(friend)',
-      'RETURN user, friend'
-    ].join('\n');
-    callback(null, query, cypher_params);
-  };
-
-  // unfriend the user
-  var _unfriend = function (params, options, callback) {
-    var cypher_params = {
-      uuid: params.uuid,
-      friend: params.friend
-    };
-
-    var query = [
-      'MATCH (user:User)-[r:friend]-(friend:User)',
-      'WHERE user.uuid={uuid} AND friend.uuid={friend}',
-      'DELETE r',
-      'RETURN user, friend'
-    ].join('\n');
-    callback(null, query, cypher_params);
-  };
-
-  // match with friends
-  var _matchWithFriends = function (params, options, callback) {
-    var cypher_params = {
-      uuid: params.uuid
-    };
-
-    var query = [
-      'MATCH (user:User)',
-      'WHERE user.uuid={uuid}',
-      'WITH user',
-      'MATCH (user)-[r?:friend]-(friend:User)',
-      'RETURN user, COLLECT(friend) as friends'
-    ].join('\n');
-    callback(null, query, cypher_params);
-  };
+  var query = [
+    'MATCH (user:User)',
+    'WHERE user.uuid={uuid}',
+    'WITH user',
+    'MATCH (user)-[r?:friend]-(friend:User)',
+    'RETURN user, COLLECT(friend) as friends'
+  ].join('\n');
+  callback(null, query, cypher_params);
+};
 
 
 
 
-  // exposed functions
+// exposed functions
 
-  return {
-    getByUUID: Cypher(_matchByUUID, _singleUser),
-    getByName: Cypher(_matchByName, _singleUser),
-    updateName: Cypher(_updateName, _singleUser),
-    create: Cypher(_create, _singleUser),
-    login: Cypher(_create, _singleUser),
-    getAll: Cypher(_matchAll, _multipleUsers),
-    friendUser: Cypher(_friend, _singleUserWithFriend),
-    unfriendUser: Cypher(_unfriend, _singleUserWithFriend),
-    deleteUser: Cypher(_delete),
-    getWithFriends: Cypher(_matchWithFriends, _singleUserWithFriends)
-  };
-
-})();
+module.exports = {
+  getByUUID: Cypher(_matchByUUID, _singleUser),
+  getByName: Cypher(_matchByName, _singleUser),
+  updateName: Cypher(_updateName, _singleUser),
+  create: Cypher(_create, _singleUser),
+  login: Cypher(_create, _singleUser),
+  getAll: Cypher(_matchAll, _multipleUsers),
+  friendUser: Cypher(_friend, _singleUserWithFriend),
+  unfriendUser: Cypher(_unfriend, _singleUserWithFriend),
+  deleteUser: Cypher(_delete),
+  getWithFriends: Cypher(_matchWithFriends, _singleUserWithFriends)
+};
--- neo4j/index.js
+++ neo4j/index.js
@@ -1,16 +1,37 @@
-module.exports = (function () {
-  // Central neo4j helper module
+// Central neo4j helper module
 
+var neo4j = require('neo4j')
+, db = new neo4j.GraphDatabase(process.env.NEO4J_URL || 'http://localhost:7474')
+, _ = require('underscore')
+;
 
-  var neo4j = require('neo4j')
-  , db = new neo4j.GraphDatabase(process.env.NEO4J_URL || 'http://localhost:7474')
-  ;
-
-  // Cypher
-
-  var _query = function (query, params, callback) {
-    db.query(query, params, callback);
+// Cypher
+// for creating cypher queries and processing the results
+var Cypher = function (queryFn, resultsFn) {
+  return function (params, options, callback) {
+    queryFn(params, options, function (err, query, cypher_params) {
+      if (err) return callback(err);
+      db.query(query, cypher_params, function (err, results) {
+        if (err || !results.length || !_.isFunction(resultsFn)) return callback(err);
+        resultsFn(results, callback);
+      });
+    });
   };
+};
 
-  return _query;
-}());
+
+/**
+ *  Util Functions
+ */
+
+var _whereTemplate = _.template('<%= nodeVar %>.<%= key %>={<%= key %>}');
+
+Cypher.where = function (nodeVar, keys) {
+  if (keys && keys.length) {
+    return 'WHERE '+_.map(keys, function (key) {
+      return _whereTemplate({nodeVar: nodeVar, key: key});
+    }).join(' AND ');
+  }
+};
+
+module.exports = Cypher;
